233797e6caa181ef54a2500601e151ab85983a96
Patrick McKinney <pnmcartography@gmail.com>
2016-07-06 13:54:27 -0400

    update meta info. clean up code

--- EstimateCensusPopulation.py
+++ EstimateCensusPopulation.py
@@ -1,118 +1,81 @@
-#-------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------#
 # Name:        Estimate Census Population for Risk Radius
-# Purpose:     Create resident population estimates for each risk radius of a
-#              SARA facility
 #
-# Author:      pmckinney
+# Purpose:     Create resident population estimates for each risk radius of a SARA facility
 #
+# Summary:     User selects a SARA risk radius layer.  For each record in that layer, the
+#              2010  U.S. Census block dataset is clipped to the risk radius.  A proportional
+#              resident population is calculated based upon how much of the block is within the
+#              risk radius.  A table is generated that calculates the total proportional population
+#              within the risk radius
+
+# Author:      Patrick McKinney#
 # Created:     03/16/2016
 # Copyright:   (c) Cumberland County GIS 2016
 # Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------#
 
 # Import arcpy
 import arcpy
 
-# Variables for Clip tool
-# SARA Facility
+# SARA Facility - feature to be clipped
 sara = arcpy.GetParameterAsText(0)
-# Clip Feature - Regional U.S. Census Blocks
-censusBlocks = r'\\CCPASR07\ncgs$\Scripts\ArcGIS Geoprocessing\SARA Tool\SARA_Tool_DEV.gdb\Regional_Census2010_Blocks_SPS'
-# Output
+# Regional U.S. Census Blocks - clipping feature
+censusBlocks = r'C:\GIS\Geodata.gdb\SARA\Regional_Census2010_Blocks_SPS'
+# Output of Clip Operation
 output = arcpy.GetParameterAsText(1)
 
 # Search cursor for SARA Facility
-#cursor = arcpy.da.SearchCursor(sara, ['Shape', 'UNITS', 'BUFFDIST'])
-cursor = arcpy.SearchCursor(sara) # old cursor syntax
-
+cursor = arcpy.SearchCursor(sara)
 try:
     # Loop through records of SARA Facility
     for row in cursor:
         # Clip US Census Blocks layer by SARA Facility record
-
         # Clip feature
         feat = row.Shape
-        #feat = row[0]
-
-        # Append buffer units and distances to output name
         # Buffer units
         buffUnits = row.UNITS
-        #buffUnits = row[1]
         # Buffer distance
         buffDist = str(int(row.BUFFDIST))
-        #buffDist = str(int(row[2]))
         # Buffer units and distance
-        buffAppend = '_' + buffUnits + '_' + buffDist
+        buffAppend = '_{}_{}'.format(buffUnits, buffDist)
         # Boiler place text for ArcPy message
-        messageText = buffDist + '-' + buffUnits + ' risk radius'
-
+        messageText = '{}-{} risk radius'.format(buffDist, buffUnits)
         # Execute Clip tool
         newInput = arcpy.Clip_analysis(censusBlocks, feat, output + buffAppend)
-
         # Add message that Clip is completed
         arcpy.AddMessage('Census Blocks clipped for ' + messageText)
-
-        # Add fields to hold results of field calculations
-
         # Add field to hold clip area to original area ratio
         areaRatioFieldName = 'AREARATIO'
         areaRatioFieldType = 'DOUBLE'
-
         # Execut Add Field tool
         arcpy.AddField_management(newInput, areaRatioFieldName, areaRatioFieldType)
-
         # Add message that Area Ratio Field has been added
         arcpy.AddMessage('Area Ratio field added for ' + messageText)
-
         # Add field to hold estimated population
         estPopFieldName = 'ESTPOP'
         estPopFieldType = 'LONG'
-
         # Execut Add Field tool
         arcpy.AddField_management(newInput, estPopFieldName, estPopFieldType)
-
         # Add message that Estimated Population Field has been added
         arcpy.AddMessage('Estimated Population field added for ' + messageText)
-
-        # Delete this after confirming it is not used
-        #areaInField = arcpy.ListFields(newInput, 'AREARATIO')[0]
-        #newArea = arcpy.ListFields(newInput, 'Shape_Area')[0]
-        #orgArea = arcpy.ListFields(newInput, 'ORAREA')[0]
-
-        # areaInField = newInput.AREARATIO
-        # orgArea = newInput.ORAREA
-        #newArea = row.Shape_Area
-        #fieldExpression = '{0} / {1}'.format(newArea, orgArea)
-        #fieldExpression = newArea / orgArea
-        #arcpy.CalculateField_management(newInput, areaInField, fieldExpression, 'PYTHON_9.3')
-
         # Calculate the new area to old area ratio for each Census Block
         areaRatioFieldExpression = '!Shape_Area! / !ORAREA!'
         arcpy.CalculateField_management(newInput, 'AREARATIO', areaRatioFieldExpression, 'PYTHON_9.3')
-
         # Add message that Area Ratio has been calculated
         arcpy.AddMessage('New area to original area ratio calculated for ' + messageText)
-
-        # Calculate the estimated population in each census block
-        # based upon the area ratio
+        # Calculate the estimated population in each census block based upon the area ratio
         estimatedPopulationFieldExpression = '!POP10! * !AREARATIO!'
         arcpy.CalculateField_management(newInput, 'ESTPOP', estimatedPopulationFieldExpression, 'PYTHON_9.3')
-
         # Add message that Estimated Population has been calculated
         arcpy.AddMessage('Estimated population calculated for ' + messageText)
-
         # Calculate the total assumed population and export to dBASE table
-    ##    arcpy.env.workspace = r'\\CCPASR07\ncgs$\Scripts\ArcGIS Geoprocessing\SARA Tool\SARA_Tool_DEV.gdb'
-        #outTableName = output + buffAppend + '_SumPop'
         outTable = output + buffAppend + '_SumPop'
         statsFields = [['ESTPOP', 'SUM']]
         arcpy.Statistics_analysis(newInput, outTable, statsFields)
-
-        # Add message that estimated population sum table create
+        # Add message that estimated population sum table created
         arcpy.AddMessage('Table of total estimated population created for ' + buffDist + '-' + buffUnits + ' risk radius')
-
     del cursor, row
-
 except Exception:
     e = sys.exc_info()[1]
     arcpy.AddError(e.args[0])
