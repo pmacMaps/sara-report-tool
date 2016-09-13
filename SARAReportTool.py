c90f0f0be5a492d4a207059da13be7188a374ca0
Patrick McKinney <pnmcartography@gmail.com>
2016-09-13 08:41:52 -0400

    add meta info to report tool script

--- SARAReportTool.py
+++ SARAReportTool.py
@@ -1,17 +1,27 @@
-#----------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------------------------------------------------#
 # Name:        SARA Reporting Tool
 #
-# Purpose:     []
+# Purpose:     To generate risk radii for a SARA facility, estimate the residential population within each risk raidus,
+#              and identify vulnerable facilities within each risk radius.
 #
-# Summary:     []
-#
+# Summary:     User enters the latitude, longitude, and PATTS ID for the SARA facility, risk radius distances and units, and the folder location
+#              for the listing of vulnerable facilities in an ArcGIS Desktop tool form.  The tool then runs three analyses: create the risk radii,
+#              estimate residential population, and extract vulnerable facilities.#
 #
 # Author:      Patrick McKinney
 # Created:     08/10/2016
-# Updated:
+#
+# Updated:     09/13/2016
+#
 # Copyright:   (c) Cumberland County GIS 2016
-# Licence:     <your licence>
-#----------------------------------------------------------------------------------------------#
+#
+# Disclaimer:  CUMBERLAND COUNTY ASSUMES NO LIABILITY ARISING FROM USE OF THESE MAPS OR DATA. THE MAPS AND DATA ARE PROVIDED WITHOUT
+#              WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#              FITNESS FOR A PARTICULAR PURPOSE.
+#              Furthermore, Cumberland County assumes no liability for any errors, omissions, or inaccuracies in the information provided regardless
+#              of the cause of such, or for any decision made, action taken, or action not taken by the user in reliance upon any maps or data provided
+#              herein. The user assumes the risk that the information may not be accurate.
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
 
 # access ArcPy module
 import arcpy
